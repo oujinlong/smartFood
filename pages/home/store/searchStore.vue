c8fdb55c986e057e262373be5566ec8457777814
zhouyr <zhouyr@asiainfo.com>
2019-05-21 17:51:16 +0800

    用户订单列表查询

--- pages/home/store/searchStore.vue
+++ pages/home/store/searchStore.vue
@@ -1,239 +1,237 @@
 <template>
 	<view>
 		<view class="search-container padding-xl">
-			<input class="search-input" confirm-type="search" focus placeholder="Key Search" placeholder-class="search-placeholder-class"
-			 @confirm="searchStoreClick" />
+			<input class="search-input" confirm-type="search" focus placeholder="Key Search" placeholder-class="search-placeholder-class" @confirm="searchStoreClick" />
 		</view>
 		<view class="bg-white padding text-black">
 			<scroll-view class="search-list" scroll-y @scrolltolower="loadMore()">
-				<block v-for="(item ,index) in storeList" :key="index">
+				<block v-for="(item, index) in storeList" :key="index">
 					<view class="uni-tab-bar-loading">
 						<view class="uni-flex uni-row" @click="storeDetailClick(item)">
 							<view style="display: flex; justify-content: center;align-items: center;">
 								<image :src="item.logo" style="width: 150upx;height: 150upx;border-radius: 50%;"></image>
 							</view>
 							<view class="uni-flex uni-column" style="padding-left: 20upx;">
-								<view class="title-name">{{item.name || '--'}}</view>
-								<view class="title-text">Restaurant Category: {{item.categoryDesc}}</view>
-								<view class="title-text">Floor,Direction: {{item.floorDesc}},{{item.directionDesc}}</view>
+								<view class="title-name">{{ item.name || '--' }}</view>
+								<view class="title-text">Restaurant Category: {{ item.categoryDesc }}</view>
+								<view class="title-text">Floor,Direction: {{ item.floorDesc }},{{ item.directionDesc }}</view>
 							</view>
 						</view>
 					</view>
 				</block>
-				<view class='no_data_container uni-flex uni-column' v-if="storeList.length == 0">
+				<view class="no_data_container uni-flex uni-column" v-if="storeList.length == 0">
 					<image src="../../../static/img/no-data.png" style="width: 300upx;height: 300upx"></image>
 					<view style="justify-content:center;font-size: 40rpx;margin-top: 20rpx ">No data</view>
 				</view>
-
 			</scroll-view>
 		</view>
 	</view>
 </template>
 
 <script>
-	import {
-		mapGetters
-	} from 'vuex'
-	export default {
-		name: '',
-		data() {
-			return {
-				storeList: [],
-				searchText: '',
-				pageSize: 10,
-				pageNo: 1,
-				currentPageNo: 1,
-				totalPage: 0,
-				floorLevelParam: [],
-				directionParam: [],
-				categoriesParam: []
+import { mapGetters } from 'vuex';
+export default {
+	name: '',
+	data() {
+		return {
+			storeList: [],
+			searchText: '',
+			pageSize: 10,
+			pageNo: 1,
+			currentPageNo: 1,
+			totalPage: 0,
+			floorLevelParam: [],
+			directionParam: [],
+			categoriesParam: []
+		};
+	},
+	props: {},
+	methods: {
+		loadMore() {
+			if (this.currentPageNo < this.totalPage) {
+				this.currentPageNo++;
+				this.pageChange(this.currentPageNo);
 			}
 		},
-		props: {},
-		methods: {
-			loadMore() {
-				if (this.currentPageNo < this.totalPage) {
-					this.currentPageNo++;
-					this.pageChange(this.currentPageNo)
-				}
-			},
-			pageChange(pageNo) {
-				const param = {
-					pageNo: pageNo,
-					pageSize: this.pageSize,
-					searchText: this.searchText
-				}
-				this.$request
-					.get("/entry/wxapp/searchStore", {
-						data: param
-					})
-					.then(res => {
-						res.store.list.forEach(storeItem => {
-							let cate = this.getCategoryFilter(storeItem.categories)
-							let floor = this.getFloorFilter(storeItem.floorLevel)
-							let direction = this.getDirectionFilter(storeItem.direction)
-							this.$set(storeItem, 'categoryDesc', cate)
-							this.$set(storeItem, 'floorDesc', floor)
-							this.$set(storeItem, 'directionDesc', direction)
-						})
-						this.storeList = [...this.storeList, ...res.store.list]
-					})
-					.catch(error => {
-						console.error('error:', error);
-					})
-			},
-			searchStoreClick(event) {
-				this.searchText = event.target.value
-				const param = {
-					pageNo: this.pageNo,
-					pageSize: this.pageSize,
-					searchText: this.searchText
-				}
-				this.$request
-					.get("/entry/wxapp/searchStore", {
-						data: param
-					})
-					.then(res => {
-						this.storeList = res.store.list
-						this.currentPageNo = res.store.currPage
-						this.totalPage = res.store.totalPage
-						this.storeList.forEach(storeItem => {
-							let cate = this.getCategoryFilter(storeItem.categories)
-							let floor = this.getFloorFilter(storeItem.floorLevel)
-							let direction = this.getDirectionFilter(storeItem.direction)
-							this.$set(storeItem, 'categoryDesc', cate)
-							this.$set(storeItem, 'floorDesc', floor)
-							this.$set(storeItem, 'directionDesc', direction)
-						})
-					})
-					.catch(error => {
-						this.storeList = []
-						console.error('error:', error);
-					});
-			},
-			getCategoryFilter(value) {
-				if (value && value !== undefined && value !== null) {
-					let list = this.categoriesParam.filter(item => {
-						if (item.paramValue === value) {
-							return item
-						}
-					})
-					return list[0].remark
-				} else {
-					return '--'
-				}
-			},
-			getDirectionFilter(value) {
-				if (value && value !== undefined && value !== null) {
-					let list = this.directionParam.filter(item => {
-						if (item.paramValue === value) {
-							return item
-						}
-					})
-					return list[0].remark
-				} else {
-					return '--'
-				}
-			},
-			getFloorFilter(value) {
-				if (value && value !== undefined && value !== null) {
-					let list = this.floorLevelParam.filter(item => {
-						if (item.paramValue === value) {
-							return item
-						}
-					})
-					return list[0].remark
-				} else {
-					return '--'
-				}
-			},
-			storeDetailClick(item) {
-				uni.navigateTo({
-					url: '/pages/home/store/index?storeId=' + item.id
+		pageChange(pageNo) {
+			const param = {
+				pageNo: pageNo,
+				pageSize: this.pageSize,
+				searchText: this.searchText
+			};
+			this.$request
+				.get('/entry/wxapp/searchStore', {
+					data: param
 				})
-			},
-			getConfig() {
-				this.floorLevelParam = []
-				this.directionParam = []
-				this.categoriesParam = []
-				this.$request
-					.get("/entry/wxapp/getParams", {
-						data: {
-							param: 'floorLevel'
-						}
-					}).then(res => {
-						this.floorLevelParam = res.params
-					})
-				this.$request
-					.get("/entry/wxapp/getParams", {
-						data: {
-							param: 'direction'
-						}
-					}).then(res => {
-						this.directionParam = res.params
-					})
-				this.$request
-					.get("/entry/wxapp/getParams", {
-						data: {
-							param: 'categories'
-						}
-					}).then(res => {
-						this.categoriesParam = res.params
-					})
+				.then(res => {
+					res.store.list.forEach(storeItem => {
+						let cate = this.getCategoryFilter(storeItem.categories);
+						let floor = this.getFloorFilter(storeItem.floorLevel);
+						let direction = this.getDirectionFilter(storeItem.direction);
+						this.$set(storeItem, 'categoryDesc', cate);
+						this.$set(storeItem, 'floorDesc', floor);
+						this.$set(storeItem, 'directionDesc', direction);
+					});
+					this.storeList = [...this.storeList, ...res.store.list];
+				})
+				.catch(error => {
+					console.error('error:', error);
+				});
+		},
+		searchStoreClick(event) {
+			this.searchText = event.target.value;
+			const param = {
+				pageNo: this.pageNo,
+				pageSize: this.pageSize,
+				searchText: this.searchText
+			};
+			this.$request
+				.get('/entry/wxapp/searchStore', {
+					data: param
+				})
+				.then(res => {
+					this.storeList = res.store.list;
+					this.currentPageNo = res.store.currPage;
+					this.totalPage = res.store.totalPage;
+					this.storeList.forEach(storeItem => {
+						let cate = this.getCategoryFilter(storeItem.categories);
+						let floor = this.getFloorFilter(storeItem.floorLevel);
+						let direction = this.getDirectionFilter(storeItem.direction);
+						this.$set(storeItem, 'categoryDesc', cate);
+						this.$set(storeItem, 'floorDesc', floor);
+						this.$set(storeItem, 'directionDesc', direction);
+					});
+				})
+				.catch(error => {
+					this.storeList = [];
+					console.error('error:', error);
+				});
+		},
+		getCategoryFilter(value) {
+			if (value && value !== undefined && value !== null) {
+				let list = this.categoriesParam.filter(item => {
+					if (item.paramValue === value) {
+						return item;
+					}
+				});
+				return list[0].remark;
+			} else {
+				return '--';
 			}
 		},
-		mounted() {
-			this.getConfig()
+		getDirectionFilter(value) {
+			if (value && value !== undefined && value !== null) {
+				let list = this.directionParam.filter(item => {
+					if (item.paramValue === value) {
+						return item;
+					}
+				});
+				return list[0].remark;
+			} else {
+				return '--';
+			}
 		},
-		computed: {
-			...mapGetters({})
+		getFloorFilter(value) {
+			if (value && value !== undefined && value !== null) {
+				let list = this.floorLevelParam.filter(item => {
+					if (item.paramValue === value) {
+						return item;
+					}
+				});
+				return list[0].remark;
+			} else {
+				return '--';
+			}
 		},
-		components: {}
-	}
+		storeDetailClick(item) {
+			uni.navigateTo({
+				url: '/pages/home/store/index?storeId=' + item.id
+			});
+		},
+		getConfig() {
+			this.floorLevelParam = [];
+			this.directionParam = [];
+			this.categoriesParam = [];
+			this.$request
+				.get('/entry/wxapp/getParams', {
+					data: {
+						param: 'floorLevel'
+					}
+				})
+				.then(res => {
+					this.floorLevelParam = res.params;
+				});
+			this.$request
+				.get('/entry/wxapp/getParams', {
+					data: {
+						param: 'direction'
+					}
+				})
+				.then(res => {
+					this.directionParam = res.params;
+				});
+			this.$request
+				.get('/entry/wxapp/getParams', {
+					data: {
+						param: 'categories'
+					}
+				})
+				.then(res => {
+					this.categoriesParam = res.params;
+				});
+		}
+	},
+	mounted() {
+		this.getConfig();
+	},
+	computed: {
+		...mapGetters({})
+	},
+	components: {}
+};
 </script>
 
 <style lang="scss">
-	.search-container {
-		background-color: $app-theme-color;
-		text-align: center;
+.search-container {
+	background-color: $app-theme-color;
+	text-align: center;
 
-		.search-input {
-			background-color: #ffffff;
-			padding: 20upx;
-			height: 80upx;
-			line-height: 80upx;
-			border-radius: 40upx;
-		}
-
-		.search-placeholder-class {
-			line-height: 80upx;
-		}
+	.search-input {
+		background-color: #ffffff;
+		padding: 20upx;
+		height: 80upx;
+		line-height: 80upx;
+		border-radius: 40upx;
 	}
 
-	.search-list {
-		height: calc(100vh - 240upx);
+	.search-placeholder-class {
+		line-height: 80upx;
 	}
+}
 
-	.title-name {
-		font-size: 30upx;
-		height: 30upx;
-		text-align: left;
-		padding-bottom: 20upx;
-	}
+.search-list {
+	height: calc(100vh - 240upx);
+}
 
-	.title-text {
-		font-size: 24upx;
-		color: #324057;
-		line-height: 32upx;
-	}
+.title-name {
+	font-size: 30upx;
+	height: 30upx;
+	text-align: left;
+	padding-bottom: 20upx;
+}
 
-	.title-address {
-		font-size: 22upx;
-		color: #80838F;
-		display: -webkit-box;
-		-webkit-box-orient: vertical;
-		-webkit-line-clamp: 1;
-		overflow: hidden;
-	}
+.title-text {
+	font-size: 24upx;
+	color: #324057;
+	line-height: 32upx;
+}
 
+.title-address {
+	font-size: 22upx;
+	color: #80838f;
+	display: -webkit-box;
+	-webkit-box-orient: vertical;
+	-webkit-line-clamp: 1;
+	overflow: hidden;
+}
 </style>
---
+++ pages/order/child/reservationOrderDetail.vue
@@ -0,0 +1,188 @@
+<template>
+	<view>
+		<view class="reservation-detail" v-for="(item, index) in reservationList" :key="index">
+			<view class="order-name border-bottom">
+				Order：{{ item.orderNum }}
+				<view class="status-name" :class="'color-' + item.state" style="float:right">{{ item.state | stateFilter }}</view>
+			</view>
+			<view class="order-content-main border-bottom uni-flex uni-row">
+				<view style="display: flex; justify-content: center;align-items: center;">
+					<image :src="item.logo" style="width: 150upx;height: 150upx;border-radius: 50%;"></image>
+				</view>
+				<view class="uni-flex uni-column" style="padding-left: 20upx;">
+					<view class="title-name">{{ item.img }}</view>
+					<view class="title-text">Arrival time：{{ item.xzDate }} {{ item.yjddDate }}</view>
+					<view class="title-text">Count of meals：{{ item.jcNum }}</view>
+				</view>
+			</view>
+			<view class="order-action uni-flex uni-row">
+				<view class="text" style="line-height: 65upx;flex: 1;">Table type：{{ item.tableTypeName }}</view>
+				<view>
+					<view class="text default-color color-red" v-if="item.ydcode !== '' && item.state === 4">Apply for refund</view>
+					<view class="text color-blue" style="margin-left: 20upx;" :class="{ 'default-color': item.state === 4 }">{{ item.state | buttonFilter1 }}</view>
+					<view class="text default-color" :class="{ 'color-red': item.state === 1 || item.state === 2 || item.state === 5 || item.state === 6 }">
+						{{ item.state | buttonFilter2 }}
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+	name: '',
+	data() {
+		return {};
+	},
+	props: {
+		reservationList: {
+			type: Array,
+			default() {
+				return [];
+			}
+		}
+	},
+	methods: {},
+	mounted() {},
+	computed: {
+		...mapGetters({})
+	},
+	filters: {
+		stateFilter(state) {
+			if (state === 1) {
+				return 'Reserving';
+			} else if (state === 2) {
+				return 'Reserved';
+			} else if (state === 4) {
+				return 'Cancelled';
+			} else if (state === 5) {
+				return 'Pending refund';
+			} else if (state === 6) {
+				return 'Refund successful';
+			} else if (state === 7) {
+				return 'Refund failed';
+			} else {
+				return '--';
+			}
+		},
+		buttonFilter1(state) {
+			if (state === 1) {
+				return 'Detail';
+			} else if (state === 2) {
+				return 'Detail';
+			} else if (state === 4) {
+				return 'Cancelled';
+			} else if (state === 5) {
+				return 'Detail';
+			} else if (state === 6) {
+				return 'Refunded';
+			} else if (state === 7) {
+				return 'Rejected';
+			} else {
+				return '--';
+			}
+		},
+		buttonFilter2(state) {
+			if (state === 1) {
+				return 'Cancel';
+			} else if (state === 2) {
+				return 'Delete';
+			} else if (state === 4) {
+				return 'Delete';
+			} else if (state === 5) {
+				return 'Pending review';
+			} else if (state === 6) {
+				return 'Refund failed';
+			} else if (state === 7) {
+				return 'Rejected';
+			} else {
+				return '--';
+			}
+		}
+	},
+	components: {}
+};
+</script>
+
+<style lang="scss" type="text/scss" scoped>
+.reservation-detail {
+	box-shadow: 0 0 10px 0 rgba(105, 126, 255, 0.15);
+	border: 1px solid #dddee1;
+	border-radius: 10upx;
+	padding: 10upx 20upx;
+	margin-bottom: 20upx;
+	.order-name {
+		.status-name {
+			color: #0097ff;
+			&.color-4 {
+				color: #999;
+			}
+			&.color-5 {
+				color: #999;
+			}
+			&.color-7 {
+				color: #ff4040;
+			}
+		}
+	}
+	.order-content-main {
+		padding: 20upx 0;
+	}
+	.order-action {
+		padding: 20upx 0 10upx;
+		.default-color {
+			float: right;
+			color: #999;
+			border: 1upx solid #999;
+			border-radius: 25upx;
+			font-size: 24upx;
+			padding: 10upx 20upx;
+			text-align: center;
+		}
+		.color-blue {
+			float: right;
+			border-radius: 25upx;
+			font-size: 24upx;
+			padding: 10upx 20upx;
+			text-align: center;
+			color: #0097ff;
+			border: 1upx solid #0097ff;
+		}
+		.color-red {
+			float: right;
+			border-radius: 25upx;
+			font-size: 24upx;
+			padding: 10upx 20upx;
+			text-align: center;
+			color: #ff4040;
+			border: 1upx solid #ff4040;
+		}
+		.delete-icon {
+			float: right;
+			color: #ff4040;
+			border: 1upx solid #ff4040;
+			border-radius: 25upx;
+			font-size: 24upx;
+			padding: 10upx 20upx;
+			text-align: center;
+		}
+		.detail-icon {
+			color: #0097ff;
+			border: 1upx solid #0097ff;
+			margin-left: 20upx;
+		}
+	}
+}
+.border-bottom {
+	border-bottom: 1rpx solid #eeeeee;
+}
+.title-name {
+	font-size: 30upx;
+	text-align: left;
+}
+.title-text {
+	color: #b0b0b0;
+}
+</style>
